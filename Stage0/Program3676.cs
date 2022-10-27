ebe9055b7366bfe8bc265728f0ff4697deb0c75c
Kobi Pollak <Ypollak@g.jct.ac.il>
2022-10-27 22:48:10 +0300

    my first commit

---
+++ Stage0/Program3676.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome3676();
+            Welcome3937();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome3937();
+        private static void Welcome3676()
+        {
+            Console.Write("Enter your name: ");
+            string user_name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", user_name);
+        }
+    }
+}
