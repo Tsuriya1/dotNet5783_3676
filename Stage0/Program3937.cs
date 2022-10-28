a23e0d6dc53867af5cd79754afcc8499189fe4bc
tsuriyamalka <tsuriya@g.jct.ac.il>
2022-10-28 11:52:53 +0300

    tsuriyas changes

---
+++ Stage0/Program3937.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Stage0
+{
+    partial class Program
+    {
+        static partial void Welcome3937()
+        {
+            Console.Write("wellcome to the project");
+        }
+    }
+}
+
